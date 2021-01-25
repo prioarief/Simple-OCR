12752d0b507d420145badcb5453d5364f40f92df
prioarief <prioariefgunawangunawan@gmail.com>
2021-01-25 11:09:54 +0700

    add validation check

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const multer = require("multer");
 const { createWorker } = require("tesseract.js");
 const worker = createWorker();
 
+app.use(cors());
 const filter = (req, file, cb) => {
   if (file.mimetype.includes("jpg")) {
     cb(null, true);
@@ -26,7 +27,6 @@ var storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
 app.use(router);
 router.get("/", (req, res) => res.send("halo"));
 router.post("/", upload.single("file"), async (req, res) => {
@@ -34,23 +34,33 @@ router.post("/", upload.single("file"), async (req, res) => {
     await worker.load();
     await worker.loadLanguage("eng");
     await worker.initialize("eng");
-    const { data: { text } } = await worker.recognize(`./${req.file.originalname}`);
-    const temp = []
-    const exc = ['=', '!', '\n']
-    text.split('').map((data) => {
-        if(data !== '=') temp.push(data)
-    })
-    console.log(text);
-    return res.send(text)
+    const {
+      data: { text },
+    } = await worker.recognize(`./${req.file.originalname}`);
+    const result = [];
+    const temp = req.body.key.split(',')
+    temp.map((tempp) =>
+      result.push({ key: tempp.trim(), value: text.toUpperCase().includes(tempp.trim().toUpperCase()) })
+    );
+    const findTrue = result.filter((e) => e.value === true);
+    let msg = ''
+    for (let i = 0; i < result.length; i++) {
+      msg += result[i].key + ' = ' + result[i].value;
+      if(i < result.length - 1) msg += ', '
+      
+    }
+    if(findTrue.length < 1){
+      fs.unlinkSync(`./${req.file.originalname}`)
+      return res.status(400).json({msg: msg})
+    }
+    
+    fs.unlinkSync(`./${req.file.originalname}`)
+    return res.json({msg: msg})
   } catch (error) {
     console.log(error);
   }
-  // fs.readFile(`./${req.file.originalname}`, (err, data) => {
-  //     if(err) return console.log(err);
-  //     console.log(data);
-  // })
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
 app.listen(2000, () => console.log("running"));
